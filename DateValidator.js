cd6439f7fd5aea27d27a895b15e993db486ebb2d
yliu1019 <lisaliu@Lisas-MBP.attlocal.net>
2014-12-07 20:36:16 -0800

    add new functions for previous/next dmy

--- DateValidator.js
+++ DateValidator.js
@@ -79,26 +79,78 @@ function DateValidator(yyyy, mm, dd) {
 	}
 
 	this.currentMonth = function() {
-
+		if(this.isCurrentMonthValid)
+			return tmp_mm;
+		else
+			return undefined;
 	}
 
 	this.previousMonth = function() {
-
+		if(this.isCurrentMonthValid) {
+			if(tmp_mm > 1)
+				return DateValidator(yyyy, tmp_mm - 1, dd);
+			else 
+				return DateValidator(this.previousYear(), 12, dd);
+		} else {
+			return undefined;
+		}
 	}
 
 	this.nextMonth = function() {
-
+		if(this.isCurrentMonthValid) {
+			if(tmp_mm < 12)
+				return DateValidator(yyyy, tmp_mm + 1, dd);
+			else 
+				return DateValidator(this.nextYear(), 1, dd);
+		} else {
+			return undefined;
+		}
 	}
 
 	this.currentDay = function() {
-
+		if(this.isCurrentDayValid)
+			return tmp_dd;
+		else
+			return undefined;
 	}
 
 	this.previousDay = function() {
-
+		if(this.isCurrentDayValid) {
+			if(tmp_dd > 1)
+				return DateValidator(yyyy, mm, tmp_dd - 1);
+			else 
+				return undefined;
+		} else {
+			return undefined;
+		}
 	}
 
 	this.nextDay = function() {
+		if(this.isCurrentDayValid) {
+			if(isCurrentMonthValid) {
+				var upper_b = 31;
+				if(this.currentMonth == 4 || this.currentMonth == 6 || this.currentMonth == 9 || this.currentMonth == 11) {
+					upper_b = 30;
+				} else if (this.currentMonth == 2) {
+					if(this.isCurrentYearValid == false || this.isCurrentYearALeapYear)
+						upper_b = 29;
+					else
+						upper_b = 28;
+				}
 
+				if(tmp_dd < upper_b)
+					return DateValidator(yyyy, mm, tmp_dd + 1);
+				else {
+					var nm = this.nextMonth();
+					return DateValidator(nm.yy, nm.mm, 1);
+				}
+			} else if(tmp_dd == 31) {
+				return this;
+			} else {
+				return DateValidator(yyyy, mm, tmp_dd + 1);
+			}
+		} else {
+			return undefined;
+		}
 	}
 }
